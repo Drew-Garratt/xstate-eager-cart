743b3ce73f7b6d8c242e14ce0e40483a885b9d2f
Drew Garratt <drew_garratt@mckinsey.com>
2023-06-08 15:39:47 +0100

    removed uneeded files post merge

--- src/lib/saleor/commerceMachine/asyncCreateCart.ts
+++
@@ -1,6 +0,0 @@
-import { type StoreMachineOptions } from '@/lib/vercelCommerce/machine';
-
-export const asyncCreateCart: StoreMachineOptions['services']['asyncCreateCart'] =
-  async () => {
-    return { cart: null };
-  };
